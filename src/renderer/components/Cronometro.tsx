0f1046944fdb50ef51d1c8a42331f2a88251832c
samuelfsilva <samukfer@gmail.com>
2022-04-22 02:39:16 -0300

    Controle de tempo melhorado

--- src/renderer/components/Cronometro.tsx
+++ src/renderer/components/Cronometro.tsx
@@ -1,87 +1,98 @@
 import React, { useState, useRef, useEffect } from "react";
-
-import { Box, Container, Grid, Typography, IconButton } from "@mui/material";
+import { Container, Grid, Typography, IconButton } from "@mui/material";
 import { PlayCircle, PauseCircle, RestartAltRounded } from "@mui/icons-material";
 
 export default function Cronometro(): JSX.Element {
+  const [inicial, setInicial] = useState(0);
+  const [control, setControl] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [timerSum, setTimerSum] = useState(0);
+  const [timer, setTimer] = useState(0);
   const [minutos, setMinutos] = useState(0);
   const [segundos, setSegundos] = useState(0);
   const [milesimos, setMilesimos] = useState(0);
-  const [isActive, setIsActive] = useState(false);
-  const [isPaused, setIsPaused] = useState(true);
-  const increment: { current: NodeJS.Timeout | null } = useRef(null);
+  
+  
+  useEffect(() => {
+    let id: NodeJS.Timeout | null = null
+
+    id = setInterval(() => {
+      setTimer((control * new Date().getTime()) - inicial as number);
+    }, 23);
+
+    return () => clearInterval(id as NodeJS.Timeout);
+  }, [inicial, control]);
 
   useEffect(() => {
-    let soma = milesimos > 999 ? 1 : 0
-    setSegundos((segundos) => segundos + soma)
-  }, [milesimos])
-
-  useEffect(() => {
-    let soma = segundos > 59 ? 1 : 0
-    setMinutos((minutos) => minutos + soma)
-  }, [segundos])
-
+      setMilesimos(timer % 1000);
+      setSegundos((timer / 1000) % 60);
+      setMinutos((timer / 1000) / 60);
+  }, [timer]);
+  
   const handleStart = () => {
-    setIsActive(true)
-    setIsPaused(false)
-    increment.current = setInterval(() => {
-      setMilesimos((milesimos) => milesimos + 18)
-      setMilesimos((milesimos) => milesimos % 1000)
-      setSegundos((segundos) => segundos % 60)
-    }, 18)
-  }
-
-  const handleResume = () => {
-    setIsPaused(false)
-    increment.current = setInterval(() => {
-      setMilesimos((milesimos) => milesimos + 18)
-      setMilesimos((milesimos) => milesimos % 1000)
-      setSegundos((segundos) => segundos % 60)
-    }, 18)
-  }
+    setIsActive(true);
+    setIsPaused(false);
+    setControl(1);
+    setInicial(new Date().getTime());
+  };
 
   const handlePause = () => {
-    clearInterval(increment.current as NodeJS.Timeout)
-    setIsPaused(true)
-  }
+    setIsPaused(true);
+    setTimerSum(timer);
+    setInicial((timer) * (-1));
+    setControl(0);
+  };
 
-  const handleStartResume = () => {
-    isPaused && isActive ? handleResume() : handleStart()
-  }
+  const handleResume = () => {
+    setIsPaused(false);
+    setControl(1);
+    setInicial(new Date().getTime() - timerSum);
+  };
 
   const handleReset = () => {
-    clearInterval(increment.current as NodeJS.Timeout)
-    setIsActive(false)
-    setIsPaused(true)
-    setMilesimos(0)
-    setSegundos(0)
-    setMinutos(0)
-  }
+    setIsActive(false);
+    setIsPaused(true);
+    setMilesimos(0);
+    setSegundos(0);
+    setMinutos(0);
+    setTimer(0);
+    setControl(0);
+    setInicial(0);
+    setTimerSum(0);
+  };
+
+  const onClick = () => {
+    if (isActive && isPaused)
+      handleResume()
+    else
+      handleStart();
+  };
 
   const formatTime = () => {
-    const miliseconds = String(milesimos).padStart(3,'0')
-    const seconds = String(segundos).padStart(2,'0')
-    const minutes = String(minutos).padStart(2,'0')
-
-    return `${minutes} : ${seconds} : ${miliseconds}`
+    return `${String(Math.trunc(minutos)).padStart(2,'0')} : ${String(Math.trunc(segundos)).padStart(2,'0')} : ${String(Math.trunc(milesimos)).padStart(3,'0')}`;
   }
 
   return (
     <Container maxWidth="md" sx={{ mt: 8 }}>
       <Grid container justifyContent="center">
-        <Typography variant="h1" textAlign="center" sx={{ mt: 8 }}>
+        <Typography variant="h1" textAlign="center" sx={{ mt: 8, userSelect: 'none' }}>
           {formatTime()}
         </Typography>
       </Grid>
       <Typography variant="h1" textAlign="center" sx={{ mt: 6 }}>
-        {isPaused ? <IconButton aria-label="play" size="large" sx={{ color: "#FFF" }} onClick={handleStartResume}>
+        {isPaused ? 
+        <IconButton aria-label="play" size="large" 
+          sx={{ color: "#FFF" }} onClick={() => onClick()}>
           <PlayCircle fontSize="inherit" />
         </IconButton> :
-        <IconButton aria-label="pause" size="large" sx={{ color: "#FFF" }} onClick={handlePause}>
+        <IconButton aria-label="pause" size="large" 
+          sx={{ color: "#FFF" }} onClick={() => handlePause()}>
           <PauseCircle fontSize="inherit" />
         </IconButton>}
         {isActive &&
-        <IconButton aria-label="restart" size="large" sx={{ color: "#FFF" }} onClick={handleReset}>
+        <IconButton aria-label="restart" size="large" 
+          sx={{ color: "#FFF" }} onClick={() => handleReset()}>
           <RestartAltRounded fontSize="inherit" />
         </IconButton>}
       </Typography>
